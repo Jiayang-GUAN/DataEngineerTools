6b45f2c5fbb115e709e2d8f38b2d2d5547758199
guanji <jiayangguan02@gmail.com>
2021-02-06 16:59:05 +0100

    update projet

---
+++ 6Evaluation/Projet/amazonSpider/pipelines.py
@@ -0,0 +1,98 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+import pymongo
+from amazonSpider.items import AmazonspiderItem
+from scrapy.exporters import JsonItemExporter
+from scrapy.utils.project import get_project_settings
+settings = get_project_settings()
+from urllib import request
+import os
+
+class AmazonspiderPipeline:
+
+    def __init__(self):
+        # self.file = open('json/produits_exporter.json', 'wb')
+        # self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        # self.exporter.start_exporting()
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+        self.collection.delete_many({})
+
+    def close_spider(self, spider):
+        # self.exporter.finish_exporting()
+        # self.file.close()
+        pass
+
+
+    def process_item(self, item, spider):
+        item['classement'] = self.clean_string(item['classement'])
+        if item['produit'] != None:
+            item['produit'] = self.clean_string(item['produit'])
+        else:
+            item['produit'] = "Cet article n'est plus disponible"
+        if item['evaluations'] != None:
+            item['evaluations'] = self.clean_string(item['evaluations'])
+        if item['prix'] != None:
+            item['prix'] = self.clean_string(item['prix'])
+        # self.exporter.export_item(item)
+        # result = self.collection.find_one({"$and":[{"classement":item['classement'], "departement":item['departement']}]})
+        # if result is None:
+        self.collection.insert(dict(item))
+        return item
+
+    def clean_string(self,string):
+        return string.replace('\n','').replace('\xa0','').strip()
+
+
+class departPipeline:
+
+    def __init__(self):
+        # self.file = open('json/departementDetails.json', 'wb')
+        # self.exporter = JsonItemExporter(self.file, encoding='utf-8', ensure_ascii=False)
+        # self.exporter.start_exporting()
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION2']]
+        self.collection.delete_many({})
+
+
+    def close_spider(self, spider):
+        # self.exporter.finish_exporting()
+        # self.file.close()
+        pass
+
+    def process_item(self, item, spider):
+        item['classement'] = self.clean_string(item['classement'])
+        if item['produit'] != None:
+            item['produit'] = self.clean_string(item['produit'])
+        else:
+            item['produit'] = "Cet article n'est plus disponible"
+        if item['evaluations'] != None:
+            item['evaluations'] = self.clean_string(item['evaluations'])
+        if item['prix'] != None:
+            item['prix'] = self.clean_string(item['prix'])
+        # self.exporter.export_item(item)
+        self.collection.insert(dict(item))
+        img_path = 'static/img/'+item['departement']
+        if not os.path.exists(img_path):
+            os.makedirs(img_path)
+        if item['img_url'] != None:
+            request.urlretrieve(item['img_url'], img_path+'/'+ item['classement'] + '.jpg')
+        return item
+
+    def clean_string(self,string):
+        return string.replace('\n','').replace('\xa0','').strip()
+
---
+++ 6Evaluation/Projet/app.py
@@ -0,0 +1,60 @@
+
+
+from flask import Flask, render_template, url_for
+import json
+import subprocess
+import pymongo
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    subprocess.check_output("scrapy crawl amazon")
+    # with open('json/produits_exporter.json', 'r', encoding='utf-8') as f:
+    #     data = json.load(f)
+    connection = pymongo.MongoClient("mongodb://localhost:27017")
+    db = connection["amazon"]
+    collection = db["les3premier"]
+    data = list(collection.find())
+
+    data_ordre = {}
+    for p in data:
+        if p['departement'] in data_ordre.keys():
+            data_ordre[p['departement']].append(p)
+        else:
+            data_ordre[p['departement']] = [p]
+
+    departement = list(data_ordre.keys())
+    return render_template('index.html', data=data_ordre, departement = departement, categories_url=categories_url)
+
+@app.route('/afficheplus/<departement>')
+def depart(departement):
+    ll = "scrapy crawl departement -a category="+departement
+    subprocess.check_output(ll)
+    # with open('json/departementDetails.json', 'r', encoding='utf-8') as f:
+    #     data = json.load(f)
+    connection = pymongo.MongoClient("mongodb://localhost:27017")
+    db = connection["amazon"]
+    collection = db["produits"]
+    data = list(collection.find())
+
+    return render_template('detail.html', data=data, departement=list(categories_url.keys())[list(categories_url.values()).index(departement)])
+
+
+categories_url = {'Amazon Launchpad': 'boost', 'Animalerie': 'pet-supplies',
+                      'Appareils Amazon et Accessoires': 'amazon-devices',
+                      'Applis et Jeux': 'mobile-apps', 'Auto et Moto': 'automotive',
+                      'Bagages': 'luggage', 'Beauté et Parfum': 'beauty',
+                      'Bijoux': 'jewelry', 'Boutique Kindle': 'digital-text',
+                      'Boutique chèques-cadeaux': 'gift-cards', 'Bricolage': 'hi',
+                      'Bébé et Puériculture': 'baby', 'CD et Vinyles': 'music',
+                      'Chaussures et Sacs': 'shoes', 'Commerce, Industrie et Science': 'industrial',
+                      'Cuisine et Maison': 'kitchen', 'DVD et Blu-ray': 'dvd',
+                      'Epicerie': 'grocery', 'Fournitures de bureau': 'officeproduct',
+                      'Gros électroménager': 'appliances', 'High-Tech': 'electronics',
+                      'Hygiène et Santé': 'hpc', 'Informatique': 'computers',
+                      'Instruments de musique et Sono': 'musical-instruments', 'Jardin': 'lawn-garden',
+                      'Jeux et Jouets': 'toys', 'Jeux vidéo': 'videogames',
+                      'Livres': 'books', 'Livres anglais et étrangers': 'english-books',
+                      'Livres audio Audible & Originals': 'audible', 'Logiciels': 'software',
+                      'Luminaires & Eclairage': 'lighting', 'Montres': 'watch',
+                      'Produits Handmade': 'handmade', 'Sports et Loisirs': 'sports', 'Vêtements': 'apparel'}
